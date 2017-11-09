036ab0b7398863ef3073e3a72fa349bc7bf7b074
Janos Dobi <j.dobi88@gmail.com>
2017-11-09 11:40:32 +0100

    initial commit w index page

---
+++ src/main/java/com/greenfox/oraclechat/OraclechatApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.oraclechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OraclechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OraclechatApplication.class, args);
+	}
+}
