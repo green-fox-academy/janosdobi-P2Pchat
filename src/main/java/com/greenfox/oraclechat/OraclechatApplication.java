68379a88548e8d6ad0e66e968ed8a026dbb064eb
Janos Dobi <j.dobi88@gmail.com>
2017-11-10 09:55:33 +0100

    minor adjustments

--- src/main/java/com/greenfox/oraclechat/OraclechatApplication.java
+++ src/main/java/com/greenfox/oraclechat/OraclechatApplication.java
@@ -1,12 +1,15 @@
 package com.greenfox.oraclechat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OraclechatApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(OraclechatApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(OraclechatApplication.class, args);
+		logger.info("--Application started");
 	}
 }
