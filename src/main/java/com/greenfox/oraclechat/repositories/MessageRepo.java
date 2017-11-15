b7ec76621bb5595c73207df56341abb819a02220
Janos Dobi <j.dobi88@gmail.com>
2017-11-15 18:07:10 +0100

    fix query

--- src/main/java/com/greenfox/oraclechat/repositories/MessageRepo.java
+++ src/main/java/com/greenfox/oraclechat/repositories/MessageRepo.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface MessageRepo extends CrudRepository<Message, Long> {
 
-    @Query(value = "SELECT * FROM message ORDER BY created_at ASC" +
+    @Query(value = "SELECT * FROM message ORDER BY timestamp ASC" +
             " LIMIT 10 OFFSET ((SELECT count(*) FROM message) - 10)", nativeQuery = true)
     Iterable<Message> listTenMostRecent();
 }
