74aa018abdbf7b7934640a231dc14e76cab63f5e
Janos Dobi <j.dobi88@gmail.com>
2017-11-15 18:29:31 +0100

    fix errormessage

--- src/main/java/com/greenfox/oraclechat/model/Status.java
+++ src/main/java/com/greenfox/oraclechat/model/Status.java
@@ -1,8 +1,8 @@
 package com.greenfox.oraclechat.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Status {
 
     private String status;
