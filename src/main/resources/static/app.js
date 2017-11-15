f632d2cfce2c3496924baad003001073556db870
Janos Dobi <j.dobi88@gmail.com>
2017-11-15 10:12:11 +0100

    websocket?

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,7 +2,7 @@ function connect() {
     var socket = new SockJS('/chat');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function () {
-        stompClient.subscribe('/topic', function (message) {
+        stompClient.subscribe('/api/message/receive', function (message) {
             console.log(message);
             window.location.reload();
         });
